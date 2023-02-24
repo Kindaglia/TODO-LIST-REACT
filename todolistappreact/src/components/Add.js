039b9219b99c30185e00d1383ab40a25635ec8cb
Kindaglia <matteodrago007@gmail.com>
2023-02-24 16:49:20 +0100

    add home con vista del localstorage

--- todolistappreact/src/components/Add.js
+++ todolistappreact/src/components/Add.js
@@ -1,11 +1,13 @@
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { Button, Typography } from '@mui/material';
+import { useNavigate } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 
 
 function Add() {
 
+  const navigate = useNavigate()
   const [title,setTitle] = useState('')
   const [desc,setDesc] =  useState('') 
 
@@ -18,18 +20,18 @@ function Add() {
   const handleSubmit = () => {
     console.log({ title, desc })
 
-    const blogs = localStorage.getItem('blogs') || []
+    const _blogs = localStorage.getItem('blogs') && localStorage.getItem('blogs').length > 0 ? JSON.parse(localStorage.getItem('blogs')) : []
 
-    localStorage.setItem('blogs', JSON.stringify([...blogs ,{title, desc}])) 
+    localStorage.setItem('blogs', JSON.stringify([..._blogs, { title, desc }]))
 
-
+    navigate('/')
 }
 
   return (<div>
      <Typography> ADD BLOG </Typography>
       <TextField value={title} onChange={(e) => handleTitleChange(e)} label="Title" variant="filled" />
       <TextField value={desc} onChange={(e) => handleDescChange(e)} label="Description" variant="filled" />  
-      <Button onClick={handleSubmit} variant="contained" > SUBMIT </Button>
+      <Button onClick={handleSubmit}  variant="contained" > SUBMIT </Button>
   </div>
    
   )
--- todolistappreact/src/components/Home.js
+++ todolistappreact/src/components/Home.js
@@ -1,8 +1,41 @@
-import React from 'react'
+import { Button } from "@mui/material"
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import DeleteIcon from '@mui/icons-material/Delete'
+import EditIcon from '@mui/icons-material/Edit';
 
 function Home() {
+
+  const navigate = useNavigate()
+  const [blogs, setBlogs] = useState([])
+
+    useEffect(() => {
+        const blogs = localStorage.getItem('blogs')
+        setBlogs(JSON.parse(blogs))
+    }, [])
+
   return (
-    <div>Home</div>
+    <>
+
+    <br /><br />
+      <Button onClick={() => {navigate('add')}} variant="contained">ADD</Button><br /><br />
+      {
+        blogs && blogs.length > 0 ?
+          blogs.map(blog => {
+            return (
+              <>
+                title - {blog.title}  <br />
+                Description - {blog.desc} <br /><br />
+              </>
+              
+            )
+              
+          
+          })
+          :
+          "No data Found"
+      }
+    </>
   )
 }
 

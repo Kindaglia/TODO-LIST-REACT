7b3388654ee508a36082c06807ec2413228839d0
Kindaglia <matteodrago007@gmail.com>
2023-02-24 15:55:46 +0100

    print in log submit

--- todolistappreact/src/components/Add.js
+++ todolistappreact/src/components/Add.js
@@ -1,13 +1,29 @@
-import React from 'react'
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import { Typography } from '@mui/material';
+import { Button, Typography } from '@mui/material';
+import React, { useEffect, useState } from "react";
+
 
 function Add() {
+
+  const [title,setTitle] = useState('')
+  const [desc,setDesc] =  useState('') 
+
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value)
+  }
+  const handleDescChange = (e) => {
+    setDesc(e.target.value)
+  }
+  const handleSubmit = () => {
+    console.log({ title, desc })
+}
+
   return (<div>
-    <Typography> ADD TODO </Typography>
-      <TextField id="outlined-basic" label="Outlined" variant="standard" />
-      <TextField id="filled-basic" label="Filled" variant="standard" />
+     <Typography> ADD BLOG </Typography>
+      <TextField value={title} onChange={(e) => handleTitleChange(e)} label="Title" variant="filled" /> <br />
+      <TextField value={desc} onChange={(e) => handleDescChange(e)} label="Description" variant="filled" />   <br />
+      <Button onClick={handleSubmit} variant="contained" > SUBMIT </Button>
   </div>
    
   )
